1548b811944c99bd496f3789728e3f7cf2538443
Kanysh <kanyshomirzak@gmail.com>
2026-09-06 22:03:55 +0500

    add Task 5 implementation and HTML/CSS structure for Lecture 1

--- Lecture_1/Task5.js
+++ Lecture_1/Task5.js
@@ -15,7 +15,7 @@ user1Copy = { ...user1 }
 user1Copy.address.city = 'Astana';
 console.log(user1); //printed Astana потому что объект address уже другой объект а не user1. Поэтому как в первом примере там берется именно ссылка на объект address
 
-user1Copy2 = { ...user1, address: { ...address }};
+user1Copy2 = { ...user1, address: { ...user1.address }};
 user1Copy2.address.city = 'Shymkent';
 console.log(user1);
 
